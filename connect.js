ccd4d61fa54ede0d1324c0dbfac365e164b30389
LukaleTyson <lukaletyson99@gmail.com>
2023-02-08 08:33:55 +0300

    Changes to be committed: 	modified:   connect.js 	modified:   index.js

--- connect.js
+++ connect.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "social",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });

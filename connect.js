7727dac838343d74559f538915e507c7bfef6301
LukaleTyson <lukaletyson99@gmail.com>
2023-02-08 23:12:19 +0300

    modified:   .env 	modified:   connect.js

--- connect.js
+++ connect.js
@@ -7,12 +7,13 @@ dotenv.config();
 //   password: process.env.MYSQLPASSWORD,
 //   database: process.env.MYSQLDATABASE,
 // });
-
+// mysql://${{ MYSQLUSER }}:${{ MYSQLPASSWORD }}@${{ MYSQLHOST }}:${{ MYSQLPORT }}/${{ MYSQLDATABASE }}
 export const db = mysql.createPool({
   connectionLimit: 10,
-  connectTimeout:20,
+  connectTimeout: 20,
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
